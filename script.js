69bd6563db7bbad49e16a6ea4c8bfe44babb034b
Ashley <ashpow25@gmail.com>
2021-05-31 10:30:07 -0700

    completed assignment

---
+++ script.js
@@ -0,0 +1,56 @@
+let link = document.getElementsByClassName("link");
+
+console.log(link);
+
+// // // This handler will be executed only once when the cursor
+// // // moves over the unordered list
+// // link.addEventListener("mouseenter", function(event) {
+// //   // highlight the mouseenter target
+// //   event.target.style.color = "purple";
+
+// //   // reset the color after a short delay
+// //   setTimeout(function() {
+// //     event.target.style.color = "";
+// //   }, 500);
+// // }, false);
+
+// // This handler will be executed every time the cursor
+// //is moved over a different list item
+// link.addEventListener("mouseover", function( event ) {
+//   // highlight the mouseover target
+//   event.target.style.color = "orange";
+
+//   // reset the color after a short delay
+//   setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// }, false);
+
+
+let test = document.getElementById("link");
+
+console.log(test);
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function( event ) {
+  // highlight the mouseenter target
+  event.target.style.color = "purple";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
