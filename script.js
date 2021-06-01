eff8432f117ddc9ecc7edd8de2d71d1bb024b9cf
Ashley <ashpow25@gmail.com>
2021-05-31 21:10:11 -0700

    added links to pages to say 'coming soon' as portfolio grows, resized styling

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let link = document.getElementById("nav");
 
-console.log(link);
+//console.log(link);
 
 
-// This handler will be executed every time the cursor
-// is moved over a different list item
+
 link.addEventListener("mouseover", function( event ) {
-    // highlight the mouseover target
+ 
     event.target.style.color = "#3B9cb9";
-  
-    // reset the color after a short delay
+
     setTimeout(function() {
       event.target.style.color = "";
-    }, 500);
+    }, 600);
   }, false);
 
